a52f983cfe73b2d57fe5f4a75ab58b3973db05d4
AnneKitsune <annekitsunefox@gmail.com>
2025-08-22 21:56:14 -0400

    add missing optimize option

--- build.zig
+++ build.zig
@@ -2,10 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("anne_dice", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
         .link_libc = true,
     });
 
